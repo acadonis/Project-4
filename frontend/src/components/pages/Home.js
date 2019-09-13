3c84198d9aff1ef1486a873072581053ce4905a6
Alexis Adonis <acadonis@gmail.com>
2019-09-13 13:04:30 +0100

    home page tidied

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -7,25 +7,34 @@ function Home() {
     <div>
       <section className="section">
         <div className="container">
-          <h1 className="title is-3">Welcome to AdonisTravel, the low carbon trip planner!</h1>
+          <h1 className="title is-2 is-spaced">Welcome to Low Carbon Europe</h1>
+          <h2 className="subtitle">Find your ideal holiday in Europe and save carbon compared to a holiday further afield! </h2>
+          <h2 className="subtitle is-spaced">
+          Europe contains a wonderful array of desitinations just waiting to be explored, and you can even add your own below! </h2>
         </div>
-      </section>
-      <section className="section">
+        <br>
+        </br>
         <div className="container">
-          <h1 className="title is-3">What would you like to do today?</h1>
+          <h2 className="title is-3">What would you like to do today?</h2>
         </div>
-      </section>
-      <section className="section">
-        <div className="columns is-multiline">
-          <div className="column">
-            <Link to={'/search'}>
-              <h1 className="title is-3">Search for a low carbon holiday!</h1>
-            </Link>
-          </div>
-          <div className="column">
-            <Link to={'/destinations/new'}>
-              <h1 className="title is-3">Add a low carbon destination!</h1>
-            </Link>
+        <br>
+        </br>
+        <div className="container">
+          <div className="columns is-multiline">
+            <div className="column">
+              <div className="box">
+                <Link to={'/search'}>
+                  <h2 className="title is-3">Search for a holiday!</h2>
+                </Link>
+              </div>
+            </div>
+            <div className="column">
+              <div className="box">
+                <Link to={'/destinations/new'}>
+                  <h2 className="title is-3">Add a destination!</h2>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </section>
