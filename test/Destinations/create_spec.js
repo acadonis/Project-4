efb961d9cabe6db32cfbc20205c99ce70b12932a
Alexis Adonis <acadonis@gmail.com>
2019-11-04 20:56:02 +0000

    1st test working

--- test/Destinations/create_spec.js
+++ test/Destinations/create_spec.js
@@ -1,47 +1,37 @@
+/* global describe, it */
 import React from 'react'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
 import Login from '../../frontend/src/components/auth/Login'
 
-describe('DestinationNew', () => {
-  it('should render two input fields')
-  const state = {
+describe('Login', () => {
+  it('should render two input fields', done => {
+    const state = {
+      formData: {
+        email: '',
+        password: ''
+      },
+      error: ''
+    }
 
-    formData: {
-      email: '',
-      password: ''
-    },
-    error: ''
-  }
-
-  component = shallow(<Login formData={state.formData} errors={state.errors} />)
-
-  expect(component.find('input').length).to.eq(2)
-
-  done()
+    const component = shallow(<Login formData={state.formData} errors={state.errors} />)
+    expect(component.find('input').length).to.eq(2)
+    done()
+  })
 })
-
-it('should populate the form', done => {
-  const state = {
-    formData: {
-      email: 'alexis@test.com',
-      password: 'test'
-    },
-    errors: ''
-  }
-
-  component = shallow(<Login formData={state.formData} errors={state.errors} />)
-
-  expect(component.find({ name: 'email'}).instance().value).to.eq('alexis@test.com')
-
-  expect(component.find({ name: 'password'}).instance().value).to.eq('test')
-  done()
-})
-
-it ('should display errors', done => {
-  const state = {
-    formData: { },
-    errors: 'Incorrect login details'
-
-  }
-})
+// it('should populate the form', done => {
+//   const state = {
+//     formData: {
+//       email: 'alexis@test.com',
+//       password: 'test'
+//     },
+//     errors: ''
+//   }
+//
+//   const component = shallow(<Login formData={state.formData} errors={state.errors} />)
+//
+//   expect(component.find({ name: 'email'}).instance().value).to.eq('alexis@test.com')
+//
+//   expect(component.find({ name: 'password'}).instance().value).to.eq('test')
+//   done()
+// })
